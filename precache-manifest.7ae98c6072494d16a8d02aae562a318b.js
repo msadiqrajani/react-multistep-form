5b22e649ea44b76f3a1e50148adf38d6e9ce0d5d
Muhammad Sadiq <msadiqrajani@gmail.com>
2020-03-10 22:15:27 +0500

    Updates

---
+++ precache-manifest.7ae98c6072494d16a8d02aae562a318b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bfaf0c0bc670296563825dd9171da2e",
+    "url": "/react-multistep-form/index.html"
+  },
+  {
+    "revision": "803fb7274a58e8fdcbbd",
+    "url": "/react-multistep-form/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "6ae6f9daf9d5e987049d",
+    "url": "/react-multistep-form/static/js/2.e3bf048f.chunk.js"
+  },
+  {
+    "revision": "6d1ae0a7201d2aa71f08b67ad045a152",
+    "url": "/react-multistep-form/static/js/2.e3bf048f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "803fb7274a58e8fdcbbd",
+    "url": "/react-multistep-form/static/js/main.b51288aa.chunk.js"
+  },
+  {
+    "revision": "3dcd0e35cb8659434c99",
+    "url": "/react-multistep-form/static/js/runtime-main.4f59b235.js"
+  }
+]);
--- src/App.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./App.css";
-import UserForm from "./Components/UserForm";
-
-function App() {
-  return <UserForm />;
-}
-
-export default App;
